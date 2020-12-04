057834640678282ef7d2767b46f4954ae163e646
mkinas888 <mkinas888@gmail.com>
2020-12-04 17:13:46 +0100

    Gabor filter added

--- resize.py
+++ resize.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 from glob import glob
 import cv2
+import math
+import numpy as np
+import sys
+from enhancer import FingerprintImageEnhancer
+
 unified_dim = (128, 128)
 ddepth = cv2.CV_16S
+image_enhancer = FingerprintImageEnhancer()
 
 for i in range(50):
     folder = './dataset/sub' + str(i+1) + '/*.jpg'
@@ -18,4 +24,8 @@ for i in range(50):
 
         img = cv2.resize(img, unified_dim, interpolation=cv2.INTER_LANCZOS4)
 
-        cv2.imwrite(j[:-3] + 'png', img)
+        if(len(img.shape) > 2):                               # convert image into gray if necessary
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        out = image_enhancer.enhance(img)     # run image enhancer
+        image_enhancer.save_enhanced_image(j[:-3] + 'png')
